3f5cff30cb1d8e6167faa3cbdeb645acaced9f0a
Jay Xiao <jayxiao@umich.edu>
2024-04-09 23:45:41 -0400

    stable algo 10 moves

--- Trader.py
+++ Trader.py
@@ -10,23 +10,28 @@ class Trader:
 
 				# Orders to be placed on exchange matching engine
         result = {}
+        buy_moves = 10
+        sell_moves = 10
         for product in state.order_depths:
+
             order_depth: OrderDepth = state.order_depths[product]
             orders: List[Order] = []
             acceptable_price = 10000  # Participant should calculate this value
             print("Acceptable price : " + str(acceptable_price))
             print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(len(order_depth.sell_orders)))
-    
+
             if len(order_depth.sell_orders) != 0:
                 best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
-                if int(best_ask) < acceptable_price:
+                if int(best_ask) < acceptable_price and buy_moves >= 0:
                     print("BUY", str(-best_ask_amount) + "x", best_ask)
+                    buy_moves -= 1
                     orders.append(Order(product, best_ask, -best_ask_amount))
     
             if len(order_depth.buy_orders) != 0:
                 best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
-                if int(best_bid) > acceptable_price:
+                if int(best_bid) > acceptable_price and sell_moves >= 0:
                     print("SELL", str(best_bid_amount) + "x", best_bid)
+                    sell_moves -= 1
                     orders.append(Order(product, best_bid, -best_bid_amount))
             
             result[product] = orders

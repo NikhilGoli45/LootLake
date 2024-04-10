0545b5101bd09c07077e45c56ae81b6bea4ec09a
Jay Xiao <jayxiao@umich.edu>
2024-04-10 00:32:25 -0400

    max moves

--- Trader.py
+++ Trader.py
@@ -19,11 +19,13 @@ class Trader:
             print("Acceptable price : " + str(acceptable_price))
             print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(len(order_depth.sell_orders)))
 
+            MAX_BUY_MOVES = 10
+            MAX_SELL_MOVES = 10
             buy_moves = 0
             sell_moves = 0
             if len(order_depth.sell_orders) != 0:
                 i = 0
-                while(buy_moves <= 10 and i < len(order_depth.sell_orders)):
+                while(buy_moves <= MAX_BUY_MOVES and i < len(order_depth.sell_orders)):
                   best_ask, best_ask_amount = list(order_depth.sell_orders.items())[i]
                   # best_ask_amount is negative
                   if -best_ask_amount < 0:  # debug
@@ -31,18 +33,18 @@ class Trader:
                   if int(best_ask) < acceptable_price:
                       print("BUY", str(-best_ask_amount) + "x", best_ask)
                       buy_moves += -best_ask_amount
-                      orders.append(Order(product, best_ask, -best_ask_amount))
+                      orders.append(Order(product, best_ask, min(MAX_BUY_MOVES,-best_ask_amount)))
                   i += 1
 
             if len(order_depth.buy_orders) != 0:
                 i = 0
-                while(sell_moves <= 10 and i < len(order_depth.buy_orders)):
+                while(sell_moves <= MAX_SELL_MOVES and i < len(order_depth.buy_orders)):
                   best_bid, best_bid_amount = list(order_depth.buy_orders.items())[i]
                   # best_bid_amount is postive
                   if int(best_bid) > acceptable_price:
                       print("SELL", str(best_bid_amount) + "x", best_bid)
                       sell_moves += best_bid_amount
-                      orders.append(Order(product, best_bid, -best_bid_amount))
+                      orders.append(Order(product, best_bid, max(-MAX_SELL_MOVES,-best_bid_amount)))
                   i += 1
             
             result[product] = orders
